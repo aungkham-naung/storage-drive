1e2b8a768fff2ba413e3665ae6d83033d3348380
Akadeha420 <aungkhamnaung2003@gmail.com>
2025-02-16 22:51:37 -0600

    file upload p1 - completed

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,7 +1,82 @@
-import React from "react";
+"use client";
 
-const FileUploader = () => {
-  return <div>FileUploader</div>;
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import Image from "next/image";
+import { useState } from "react";
+import { getFileType, convertFileToUrl } from "@/lib/utils";
+import Thumbnail from "./Thumbnail";
+
+interface Props {
+  ownerId: string;
+  accountId: string;
+  className?: string;
+}
+
+const FileUploader = ({ ownerId, accountId, className }: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()} className="cursor-pointer">
+      <input {...getInputProps()} />
+      <Button type="button" className={cn("uploader-button", className)}>
+        <Image
+          src="/assets/icons/upload.svg"
+          alt="upload"
+          width={24}
+          height={24}
+        />
+        <p>Upload</p>
+      </Button>
+
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          <h4 className="h4 text-light-100">Uploading</h4>
+          {files.map((file, index) => {
+            const { type, extension } = getFileType(file.name);
+            return (
+              <li
+                key={`${file.name}-${index}`}
+                className="uploader-preview0item"
+              >
+                <div className="flex items-center gap-3">
+                  <Thumbnail
+                    type={type}
+                    extension={extension}
+                    url={convertFileToUrl(file)}
+                  />
+
+                  <div className="preview-item-name">
+                    {file.name}
+
+                    <Image
+                      src="/assets/icons/file-loader.gif"
+                      alt="Loader"
+                      width={80}
+                      height={26}
+                      unoptimized
+                    />
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
 };
 
 export default FileUploader;

f5fc5a64b159864b81121c69119fb3ed7e1b9b95
Akadeha420 <aungkhamnaung2003@gmail.com>
2025-02-22 01:18:48 -0600

    file details implemented

--- app/components/DropDown.tsx
+++ app/components/DropDown.tsx
@@ -29,6 +29,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { renameFile } from "@/lib/actions/file.actions";
 import { usePathname } from "next/navigation";
+import { FileDetails } from "./FileContentModal";
 
 const DropDown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -92,6 +93,8 @@ const DropDown = ({ file }: { file: Models.Document }) => {
               onChange={(e) => setName(e.target.value)}
             />
           )}
+
+          {value === "details" && <FileDetails file={file} />}
         </DialogHeader>
         {["rename", "delete", "share"].includes(value) && (
           <DialogFooter className="flex flex-col gap-3 md:flex-row">
